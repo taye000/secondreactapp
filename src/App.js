797a5b41bfec481f44e0e17ad5d1e75cda4c15b5
taye000 <taylorgitari@gmail.com>
2022-01-24 13:31:49 +0300

    update db when ui is updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(true);
   const [tasks, setTasks] = useState([]);
 
-  //Fetch data from DB.json file
+  //Fetch tasks from DB.json file
   useEffect(() => {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks();
@@ -16,8 +16,16 @@ function App() {
     };
     getTasks();
   }, []);
+  //fetch tasks
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+  //fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
 
     return data;
@@ -50,10 +58,22 @@ function App() {
   };
 
   //Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const upTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(upTask),
+    });
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
