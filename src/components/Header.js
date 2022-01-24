26398d604478b6ee1574e42296ea60f6059727af
taye000 <taylorgitari@gmail.com>
2022-01-24 16:45:24 +0300

    routing complete with auto hide submit button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <input
-        type="submit"
-        className="btn"
-        color="green"
-        text="Add"
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <input
+          type="submit"
+          className="btn"
+          color="green"
+          text="Add"
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
